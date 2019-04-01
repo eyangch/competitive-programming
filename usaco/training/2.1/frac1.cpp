c90c7a9ae5b1e593122153e0e15d374cb4bb2d06
eyangch <45834647+eyangch@users.noreply.github.com>
2019-03-31 20:32:40 -0500

    Add files via upload

--- usaco/training/2.1/frac1.cpp
+++ usaco/training/2.1/frac1.cpp
@@ -1,1 +1,81 @@
+/*
+ID: sireric1
+TASK: frac1
+LANG: C++11
+*/
 
+#include <iostream>
+#include <fstream>
+#include <cmath>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+struct fraction{
+    int n;
+    int d;
+};
+
+bool cmpFrac(fraction x, fraction y){
+    return (x.n/(float)x.d) < (y.n/(float)y.d);
+}
+
+bool isRelPrime(int x, int y){
+    int xn = sqrt(x);
+    int yn = sqrt(y);
+    vector<int> xl, yl;
+    bool yesx = false;
+    bool yesy = false;
+    for(int i = 1; i <= xn; i++){
+        if(x % i == 0){
+            xl.push_back(i);
+            xl.push_back(x/i);
+            yesx = true;
+        }
+    }
+    for(int i = 1; i <= yn; i++){
+        if(y % i == 0){
+            yl.push_back(i);
+            yl.push_back(y/i);
+            yesy = true;
+        }
+    }
+
+    if((!yesx || !yesy) && y != 1){
+        return false;
+    }
+
+    for(int i = 0; i < xl.size(); i++){
+        for(int j = 0; j < yl.size(); j++){
+            if(xl[i] == yl[j] && (xl[i] != 1 || x == y || x == 0) && x != 1 && y != 1){
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+int main(){
+    ifstream fin ("frac1.in");
+    ofstream fout ("frac1.out");
+    int N;
+    vector<fraction> fracs;
+    fin >> N;
+
+    for(int i = 1; i <= N; i++){
+        for(int j = 0; j <= i; j++){
+            if(isRelPrime(j, i)){
+                fraction tmp;
+                tmp.d = i;
+                tmp.n = j;
+                fracs.push_back(tmp);
+            }
+        }
+    }
+    sort(fracs.begin(), fracs.end(), cmpFrac);
+    for(int i = 0; i < fracs.size(); i++){
+            fout << fracs[i].n << "/" << fracs[i].d << endl;
+    }
+    return 0;
+}
