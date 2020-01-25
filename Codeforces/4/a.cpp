1e2ca0ea3f99dae61c5c410c3bc92263c505a107
eyangch <eyangch@gmail.com>
2020-01-24 18:05:06 -0600

    Add Codeforces 4A

---
+++ Codeforces/4/a.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int N; cin >> N;
+    if(N % 2 == 0 && N > 2) cout << "YES" << endl;
+    else cout << "NO" << endl;
+    return 0;
+}
