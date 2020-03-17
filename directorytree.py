2a6bc6f9bfd5670af3a666ce738657eb7506200a
eyangch <eyangch@gmail.com>
2020-03-17 18:29:50 -0500

    Update README

---
+++ directorytree.py
@@ -0,0 +1,16 @@
+import os
+totalfiles = 0
+arr = []
+for root, dirs, files in os.walk("."):
+    if "directorytree.py" in files:
+        files.remove("directorytree.py")
+    if "README.md" in files:
+        files.remove("README.md")
+    for i in files:
+        arr.append("[" + root + "/" + i + "](" + root + "/" + i + ")")
+        totalfiles += 1
+    if ".git" in dirs:
+        dirs.remove(".git")
+print("Total Files: " + str(totalfiles))
+for i in arr:
+    print(i)
