ff167b4f5218a8790d0005a99ff5f47e93f269af
eyangch <eyangch@gmail.com>
2020-03-19 16:34:38 -0500

    Make modifying README automatic

--- directorytree.py
+++ directorytree.py
@@ -3,14 +3,27 @@ arr = []
 for root, dirs, files in os.walk("."):
     dirs.sort()
     files.sort()
-    if "directorytree.py" in files:
-        files.remove("directorytree.py")
-    if "README.md" in files:
-        files.remove("README.md")
-    for i in files:
-        arr.append("[" + root + "/" + i + "](" + root + "/" + i + ")")
+    if root != ".":
+        for i in files:
+            arr.append("[" + root + "/" + i + "](" + root + "/" + i + ")")
     if ".git" in dirs:
         dirs.remove(".git")
-print("### Total Files: " + str(len(arr)))
-for i in arr:
-    print(i + "\\")
+
+rread = open("README.md")
+storelines = []
+for line in rread:
+    if len(line) >= 16 and line[:16] == "### Total Files:":
+        break
+    storelines.append(line)
+rread.close()
+
+rwrite = open("README.md", mode="w")
+for line in storelines:
+    rwrite.write(line)
+
+rwrite.write("### Total Files: " + str(len(arr)) + "\n")
+for i in arr[:-1]:
+    rwrite.write(i + "\\\n")
+rwrite.write(arr[len(arr) - 1] + "\n")
+
+rwrite.close()
