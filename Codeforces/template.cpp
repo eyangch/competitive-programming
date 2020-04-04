114f64c92e0023e0ce5b44f2294fe7cea54f4c0b
eyangch <eyangch@gmail.com>
2020-04-04 09:30:49 -0500

    Update template

--- Codeforces/template.cpp
+++ Codeforces/template.cpp
@@ -6,15 +6,16 @@ typedef long long ll;
 typedef vector<int> vi;
 typedef pair<int, int> pii;
 template <typename T1, typename T2>
-ostream &operator <<(ostream &os, pair<T1, T2> p){os << p.first << " " << p.second;}
+ostream &operator <<(ostream &os, pair<T1, T2> p){os << p.first << " " << p.second; return os;}
 template <typename T>
-ostream &operator <<(ostream &os, vector<T> v){for(T i : v) os << i << " ";}
+ostream &operator <<(ostream &os, vector<T> &v){for(T i : v)os << i << ", "; return os;}
 template <typename T>
-ostream &operator <<(ostream &os, set<T> s){for(T i : s) os << i << " ";}
+ostream &operator <<(ostream &os, set<T> s){for(T i : s) os << i << ", "; return os;}
 template <typename T1, typename T2>
-ostream &operator <<(ostream &os, map<T1, T2> m){for(pair<T1, T2> i : m) os << i << endl;}
+ostream &operator <<(ostream &os, map<T1, T2> m){for(pair<T1, T2> i : m) os << i << endl; return os;}
 
 int main(){
-
+      
     return 0;
 }
+
