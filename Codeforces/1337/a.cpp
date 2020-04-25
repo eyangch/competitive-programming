d9453dcd594a06ad2c43ef245564781773ae2b4a
eyangch <eyangch@gmail.com>
2020-04-25 10:07:31 -0500

    Add Codeforces 1337A

---
+++ Codeforces/1337/a.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int, int> pii;
+template <typename T1, typename T2>
+ostream &operator <<(ostream &os, pair<T1, T2> p){os << p.first << " " << p.second; return os;}
+template <typename T>
+ostream &operator <<(ostream &os, vector<T> &v){for(T i : v)os << i << ", "; return os;}
+template <typename T>
+ostream &operator <<(ostream &os, set<T> s){for(T i : s) os << i << ", "; return os;}
+template <typename T1, typename T2>
+ostream &operator <<(ostream &os, map<T1, T2> m){for(pair<T1, T2> i : m) os << i << endl; return os;}
+
+int main(){
+    int t; cin >> t;
+    while(t--){
+        int a, b, c, d; cin >> a >> b >> c >> d;
+        cout << b << " " << max(c + 1 - b, b) << " " << c << endl; 
+    }
+    return 0;
+}
+
+
--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # competitive-programming
 A bunch of coding stuff
 
-### Total Files: 185
+### Total Files: 186
 [./Codeforces/template.cpp](./Codeforces/template.cpp)\
 [./Codeforces/1/a.cpp](./Codeforces/1/a.cpp)\
 [./Codeforces/10/a.cpp](./Codeforces/10/a.cpp)\
@@ -49,6 +49,7 @@ A bunch of coding stuff
 [./Codeforces/1330/a.cpp](./Codeforces/1330/a.cpp)\
 [./Codeforces/1330/b.cpp](./Codeforces/1330/b.cpp)\
 [./Codeforces/1335/d.cpp](./Codeforces/1335/d.cpp)\
+[./Codeforces/1337/a.cpp](./Codeforces/1337/a.cpp)\
 [./Codeforces/1339/a.cpp](./Codeforces/1339/a.cpp)\
 [./Codeforces/1339/b.cpp](./Codeforces/1339/b.cpp)\
 [./Codeforces/1339/c.cpp](./Codeforces/1339/c.cpp)\
