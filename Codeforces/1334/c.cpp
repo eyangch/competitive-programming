fa670ac0317b43c92717f52f582674bdb83f4f50
eyangch <eyangch@gmail.com>
2020-04-25 19:45:47 -0500

    Add Codeforces 1334C

--- Codeforces/1334/c.cpp
+++ Codeforces/1334/c.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 typedef long long ll;
 typedef vector<int> vi;
-typedef pair<int, int> pii;
+typedef pair<ll, ll> pii;
 template <typename T1, typename T2>
 ostream &operator <<(ostream &os, pair<T1, T2> p){os << p.first << " " << p.second; return os;}
 template <typename T>
@@ -16,7 +16,30 @@ ostream &operator <<(ostream &os, map<T1, T2> m){for(pair<T1, T2> i : m) os << i
 
 int main(){
     ios_base::sync_with_stdio(false);
-    cin.tie(NULL); 
+    cin.tie(NULL);
+    int t; cin >> t;
+    while(t--){
+        int n; cin >> n;
+        pii a[n];
+        for(int i = 0; i < n; i++){
+            cin >> a[i].first >> a[i].second;
+        }
+        ll bm[n];
+        ll pt = 0;
+        for(int i = 1; i < n; i++){
+            bm[i] = max(0ll, a[i].first - a[i-1].second);
+        }
+        bm[0] = max(0ll, a[0].first - a[n-1].second);
+        for(int i = 0; i < n; i++){
+            pt += bm[i];
+        }
+        ll m = LLONG_MIN;
+        for(int i = 0; i < n; i++){
+            m = max(m, -a[i].first + bm[i]);
+        }
+        cout << pt - m << endl;
+    }
     return 0;
 }
+
 
--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # competitive-programming
 A bunch of coding stuff
 
-### Total Files: 191
+### Total Files: 192
 [./Codeforces/template.cpp](./Codeforces/template.cpp)\
 [./Codeforces/1/a.cpp](./Codeforces/1/a.cpp)\
 [./Codeforces/10/a.cpp](./Codeforces/10/a.cpp)\
@@ -50,6 +50,7 @@ A bunch of coding stuff
 [./Codeforces/1330/b.cpp](./Codeforces/1330/b.cpp)\
 [./Codeforces/1334/a.cpp](./Codeforces/1334/a.cpp)\
 [./Codeforces/1334/b.cpp](./Codeforces/1334/b.cpp)\
+[./Codeforces/1334/c.cpp](./Codeforces/1334/c.cpp)\
 [./Codeforces/1335/d.cpp](./Codeforces/1335/d.cpp)\
 [./Codeforces/1337/a.cpp](./Codeforces/1337/a.cpp)\
 [./Codeforces/1337/b.cpp](./Codeforces/1337/b.cpp)\
