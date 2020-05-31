5e87121ed672308285a4e5a3b2d702e4b2377638
eyangch <eyangch@gmail.com>
2020-05-30 19:29:08 -0500

    Add Codeforces 1359B

---
+++ Codeforces/1359/b.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int, int> pii;
+template <typename T1, typename T2>
+ostream &operator <<(ostream &os, pair<T1, T2> p){os << p.first << " " << p.second; return os;}
+template <typename T>
+ostream &operator <<(ostream &os, vector<T> &v){for(T i : v)os << i << ", "; return os;}
+template <typename T>
+ostream &operator <<(ostream &os, set<T> s){for(T i : s) os << i << ", "; return os;}
+template <typename T1, typename T2>
+ostream &operator <<(ostream &os, map<T1, T2> m){for(pair<T1, T2> i : m) os << i << endl; return os;}
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t; cin >> t;
+    while(t--){
+        int n, m, x, y; cin >> n >> m >> x >> y;
+        string sq[n];
+        int ans = 0;
+        for(int i = 0; i < n; i++){
+            cin >> sq[i];
+            for(int j = 0; j < m; j++){
+                if(sq[i][j] == '.'){
+                    if(y < 2 * x && j < m-1 && sq[i][j+1] == '.'){
+                        ans += y;
+                        j++;
+                    }else{
+                        ans += x;
+                    }
+                }
+            }
+        }
+        cout << ans << endl;
+    }
+    return 0;
+}
+
+
