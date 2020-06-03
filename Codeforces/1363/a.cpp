85f6091d037ff6131b3f460236e2ebf3fa5051c9
eyangch <eyangch@gmail.com>
2020-06-02 20:15:38 -0500

    Add Codeforces 1363A

---
+++ Codeforces/1363/a.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int, int> pii;
+template <typename T1, typename T2>
+ostream &operator <<(ostream &os, pair<T1, T2> p){os << p.first << " " << p.second; return os;}
+template <typename T>
+ostream &operator <<(ostream &os, vector<T> &v){for(T i : v)os << i << ", "; return os;}
+template <typename T>
+ostream &operator <<(ostream &os, set<T> s){for(T i : s) os << i << ", "; return os;}
+template <typename T1, typename T2>
+ostream &operator <<(ostream &os, map<T1, T2> m){for(pair<T1, T2> i : m) os << i << endl; return os;}
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t; cin >> t;
+    while(t--){
+        int n, x; cin >> n >> x;
+        int odd = 0, even = 0;
+        for(int i = 0; i < n; i++){
+            int a; cin >> a;
+            if(a % 2 == 0){
+                even++;   
+            }else{
+                odd++;
+            }   
+        }
+        if(odd > 0 && (x < n || odd % 2 == 1) && (even > 0 || x % 2 == 1)){
+            cout << "Yes" << endl;
+        }else{
+            cout << "No" << endl;
+        }
+    }
+    return 0;
+}
+
+
