047e87e0a10238a9e2eeaef92a9042e012b419fb
eyangch <eyangch@gmail.com>
2020-07-03 08:30:52 -0500

    Add Codeforces 1348D

---
+++ Codeforces/1348/d.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int, int> pii;
+template <typename T1, typename T2>
+ostream &operator <<(ostream &os, pair<T1, T2> p){os << p.first << " " << p.second; return os;}
+template <typename T>
+ostream &operator <<(ostream &os, vector<T> &v){for(T i : v)os << i << ", "; return os;}
+template <typename T>
+ostream &operator <<(ostream &os, set<T> s){for(T i : s) os << i << ", "; return os;}
+template <typename T1, typename T2>
+ostream &operator <<(ostream &os, map<T1, T2> m){for(pair<T1, T2> i : m) os << i << endl; return os;}
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int T; cin >> T;
+    while(T--){
+        int N; cin >> N;
+        int d = floor(log2(N));
+        cout << d << endl;
+        int nbac = 1;
+        N -= d+1;
+        for(int i = d; i > 0; i--){
+            int x = min(N / i, nbac);
+            N -= x * i;
+            cout << x << " ";
+            nbac += x;
+        }
+        cout << endl;
+    }
+    return 0;
+}
+
+
