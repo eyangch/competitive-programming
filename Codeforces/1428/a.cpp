b033f626e80e74dc4cdf6f342eb50b39e91d5151
eyangch <eyangch@gmail.com>
2020-10-17 14:36:16 -0500

    Add Codeforces 1428A

---
+++ Codeforces/1428/a.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+#define f first
+#define s second
+
+using namespace std;
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int, int> pii;
+template <typename T1, typename T2>
+ostream &operator <<(ostream &os, pair<T1, T2> p){os << p.first << " " << p.second; return os;}
+template <typename T>
+ostream &operator <<(ostream &os, vector<T> &v){for(T i : v)os << i << ", "; return os;}
+template <typename T>
+ostream &operator <<(ostream &os, set<T> s){for(T i : s) os << i << ", "; return os;}
+template <typename T1, typename T2>
+ostream &operator <<(ostream &os, map<T1, T2> m){for(pair<T1, T2> i : m) os << i << endl; return os;}
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int T; cin >> T;
+    while(T--){
+        int x1, y1, x2, y2; cin >> x1 >> y1 >> x2 >> y2;
+        int ans = abs(x1 - x2) + abs(y1 - y2);
+        if(x1 != x2 && y1 != y2){
+            ans += 2;   
+        }
+        cout << ans << endl;
+    }
+    return 0;
+}
+
+
--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # competitive-programming
 A bunch of coding stuff
 
-### Total Files: 508
+### Total Files: 509
 [./CEOI/2017/bet.cpp](./CEOI/2017/bet.cpp)\
 [./CEOI/2020/fancyfence.cpp](./CEOI/2020/fancyfence.cpp)\
 [./CSES/IntroductoryProblems/appledivision.cpp](./CSES/IntroductoryProblems/appledivision.cpp)\
@@ -238,6 +238,7 @@ A bunch of coding stuff
 [./Codeforces/1426/e.cpp](./Codeforces/1426/e.cpp)\
 [./Codeforces/1427/c.cpp](./Codeforces/1427/c.cpp)\
 [./Codeforces/1427/d.cpp](./Codeforces/1427/d.cpp)\
+[./Codeforces/1428/a.cpp](./Codeforces/1428/a.cpp)\
 [./Codeforces/152/c.cpp](./Codeforces/152/c.cpp)\
 [./Codeforces/160/a.cpp](./Codeforces/160/a.cpp)\
 [./Codeforces/166/a.cpp](./Codeforces/166/a.cpp)\
