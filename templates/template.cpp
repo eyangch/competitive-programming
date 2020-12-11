9432ab7aa1adb7d055ab228b30e6a853bb8c383f
eyangch <eyangch@gmail.com>
2020-12-10 19:50:30 -0600

    Update template

--- templates/template.cpp
+++ templates/template.cpp
@@ -17,9 +17,16 @@ ostream &operator <<(ostream &os, set<T> s){for(T i : s) os << i << ", "; return
 template <typename T1, typename T2>
 ostream &operator <<(ostream &os, map<T1, T2> m){for(pair<T1, T2> i : m) os << i << endl; return os;}
 
+void setIO(string s){
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+    if(s != ""){
+        freopen((s+".in").c_str(), "r", stdin);
+        freopen((s+".out").c_str(), "w", stdout);
+    }
+}
+
 signed main(){
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL); 
+    setIO("");
     return 0;
 }
 
