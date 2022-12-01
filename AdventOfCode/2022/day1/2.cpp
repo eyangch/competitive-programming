bea3f4ff687f367397263abded03c2b23bf702d8
eyangch <eyangch@gmail.com>
2022-11-30 23:06:54 -0600

    Add Advent of Code 2022 day 1

---
+++ AdventOfCode/2022/day1/2.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+#define endl '\n'
+#define eat cin
+#define moo cout
+#define int long long
+
+using namespace std;
+
+vector<int> f;
+
+int32_t main(){
+	eat.tie(0) -> sync_with_stdio(0);
+	freopen("input.txt", "r", stdin);
+	string l;
+	int cur = 0;
+	while(getline(cin, l)){
+		if(l.size() == 0){
+			f.push_back(cur);
+			cur = 0;
+		}else{
+			cur += stoi(l);
+		}
+	}
+	f.push_back(cur);
+	sort(f.begin(), f.end(), greater<int>());
+	moo << f[0]+f[1]+f[2] << endl;
+}
